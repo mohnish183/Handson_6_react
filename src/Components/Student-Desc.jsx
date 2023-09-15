16d536f79df88f22936587004058d16b89149056
mohnish183 <51234174+mohnish183@users.noreply.github.com>
2023-09-15 19:18:19 +0530

    Update Student-Desc.jsx

--- src/Components/Student-Desc.jsx
+++ src/Components/Student-Desc.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./studentdesc.css";
-import { useNavigate } from "react-router-dom";
+
 import StudentDescForm from "./StudentDescForm";
 function StudentDesc(props) {
   const saveStudentDataHandler = (enteredStudentData) => {
